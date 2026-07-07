aa3acc7047ecf21cce7bf1480df6e0dba45f943f
dutch25 <haminhduccp@gmail.com>
2026-07-07 17:10:27 +0700

    Add private3 section for VinaHentai

--- src/VinaHentai/VinaHentai.ts
+++ src/VinaHentai/VinaHentai.ts
@@ -19,7 +19,7 @@ import { Parser } from './VinaHentaiParser'
 
 const BASE_URL = 'https://vinahentai.one'
 export const VinaHentaiInfo: SourceInfo = {
-    version: '1.1.14',
+    version: '1.1.15',
     name: 'VinaHentai',
     icon: 'icon.png',
     author: 'Dutch25',
@@ -75,6 +75,7 @@ export class VinaHentai extends Source {
             { id: 'month', title: 'Top Tháng', url: `${BASE_URL}/leaderboard/manga?period=monthly` },
             { id: 'private', title: 'Bộ Sưu Tập Riêng', url: `${BASE_URL}/genres/anal` },
             { id: 'private2', title: 'Bộ Sưu Tập Riêng 2', url: `${BASE_URL}/genres/anal` },
+            { id: 'private3', title: 'Bộ Sưu Tập Riêng 3', url: `${BASE_URL}/genres/anal` },
             { id: 'cosplay', title: 'Ảnh Cosplay', url: `${BASE_URL}` },
         ]
 
@@ -90,8 +91,8 @@ export class VinaHentai extends Source {
         for (const section of sections) {
             try {
                 let manga: PartialSourceManga[] = []
-                if (section.id === 'private' || section.id === 'private2') {
-                    const sortParam = section.id === 'private2' ? '&sort=viewNumber' : '';
+                if (section.id === 'private' || section.id === 'private2' || section.id === 'private3') {
+                    const sortParam = section.id === 'private2' ? '&sort=viewNumber' : section.id === 'private3' ? '&sort=likeNumber' : '';
                     try {
                         const response = await this.requestManager.schedule(
                             App.createRequest({ url: `${BASE_URL}/search/advanced?apply=1&excludeGenres=yaoi%2Ctrap%2Cfutanari%2Cfurry&includeGenres=anal%2Ckhong-che&page=1&status=${sortParam}`, method: 'GET' }), 0
@@ -136,8 +137,8 @@ export class VinaHentai extends Source {
         const page = metadata?.page ?? 1
         let url = `${BASE_URL}/danh-sach?page=${page}`
 
-        if (homepageSectionId === 'private' || homepageSectionId === 'private2') {
-            const sortParam = homepageSectionId === 'private2' ? '&sort=viewNumber' : '';
+        if (homepageSectionId === 'private' || homepageSectionId === 'private2' || homepageSectionId === 'private3') {
+            const sortParam = homepageSectionId === 'private2' ? '&sort=viewNumber' : homepageSectionId === 'private3' ? '&sort=likeNumber' : '';
             try {
                 const response = await this.requestManager.schedule(
                     App.createRequest({ url: `${BASE_URL}/search/advanced?apply=1&excludeGenres=yaoi%2Ctrap%2Cfutanari%2Cfurry&includeGenres=anal%2Ckhong-che&page=${page}&status=${sortParam}`, method: 'GET' }), 0
